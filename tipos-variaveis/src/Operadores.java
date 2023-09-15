180f14c3d1e138f284363f5f40e1a6d5a4dbf358
jvodev <i8chromed@hotmail.com>
2023-09-14 22:49:52 -0300

    concatenação

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,31 @@
+public class Operadores {
+    public static void main(String[] args)  {
+        String nomeCompleto = "LINGUAGEM" + "JAVA"; // CONCATENAÇÃO 
+        System.out.println(nomeCompleto);
+		
+                                //qual o resultado das expressoes abaixo?//
+
+                            String concatenacao ="?"; 
+
+                        concatenacao = 1+1+1+"1"; // 31
+        
+                    System.out.println(concatenacao);
+
+                concatenacao = 1+"1"+1+1; // 1111
+
+            System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1"; /// 1111
+
+    System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1; /// 1111
+
+            System.out.println(concatenacao);
+
+                concatenacao = "1"+(1+1+1); /// 13
+
+                    System.out.println(concatenacao);
+
+    }
+}
