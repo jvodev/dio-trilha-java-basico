e915ed3128ece3be32c0a8ba239df9e066241c69
jvodev <i8chromed@hotmail.com>
2023-09-14 23:04:09 -0300

    incrementador basics

---
+++ tipos-variaveis/src/Incrementadores.java
@@ -0,0 +1,11 @@
+public class Incrementadores {
+    public static void main(String[] args)  {
+        int numero = 5;
+
+        int numeroSecundario = 10;
+
+        int teste = ++numeroSecundario + ++numero; // teste, curiosidade apenas
+
+        System.out.println(teste);
+    }
+}
