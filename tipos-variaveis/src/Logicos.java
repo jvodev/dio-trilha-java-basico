a8acdadfd653f12df0e71ea1adab8e73657d8419
jvodev <i8chromed@hotmail.com>
2023-09-17 14:56:25 -0300

    novo commit a respeito da lógica de java :)

---
+++ tipos-variaveis/src/Logicos.java
@@ -0,0 +1,20 @@
+public class Logicos {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2) {
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2) {
+            System.out.println("Uma das condições são verdadeiras");
+        }
+
+        if(condicao1 && ( 7 > 4 )) {
+            System.out.println("X");
+        }
+
+            System.out.println("fim");
+    }
+}
---
+++ tipos-variaveis/src/Ternario.java
@@ -0,0 +1,19 @@
+public class Ternario {
+    
+    public static void main (String[] arg)  {
+
+            int a, b;
+            a = 5;
+            b = 6;
+
+            String resultado = a==b ?"verdadeiro" : "falso";
+
+            //String resultado = "";
+            //if (a==b)
+            //   resultado = "verdadeiro";
+            // else
+            //    resultado = "falso";
+            //     
+                System.out.println(resultado);
+        }
+     }
